f9909fb620af07862dbd1b17762e4e641123e8c0
Elijah Millard <emillard@icstars.org>
2026-02-23 14:34:22 -0600

    add new functions
    streamline calculate total

--- Errors & Debugging/index.js
+++ Errors & Debugging/index.js
@@ -1,33 +1,74 @@
 // STARTER CODE
 
+// function calculateTotal(numbers) {
+//  let total = 0;
+//  for (let i = 0; i <= numbers.length - 1; i++) { // BUG: <= causes undefined
+//    total += numbers[i];
+//   //  console.log(total);
+//  }
+// //  console.log(typeof total);
+// //  console.log("returning:", total, typeof total);
+//  return total;
+// }
+
 function calculateTotal(numbers) {
- let total = 0;
- for (let i = 0; i <= numbers.length /*- 1*/; i++) { // BUG: <= causes undefined
-   total += numbers[i];
-  //  console.log(total);
- }
-//  console.log(typeof total);
-//  console.log("returning:", total, typeof total);
- return total;
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
 function calculateAverage(numbers){
   if(!numbers || numbers.length === 0) throw Error("cannot calculate ave of empty array")
-  const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
   return (sum / numbers.length);
 }
+function findPositive(numbers){
+  if(!numbers || numbers.length === 0) throw Error("cannot find pos of empty array")
+  const positiveNumbers = numbers.filter(number => number > 0);
+  return positiveNumbers;
+}
 
+function findMax(numbers){
+    const max = numbers.reduce((accumulator, currentValue) => {
+    return Math.max(accumulator, currentValue);
+  }, -Infinity);
+  return max;
+}
 
-
-const data1 = [1, 2, 3];
+const data1 = [-1, 2, -3];
 const data2 = [4, 5, 6];
+
+try {
+  console.log(calculateTotal(data1))
+  console.log(calculateAverage(data1))
+  console.log(findPositive(data1))
+  console.log(findMax(data1))
+
+  console.log(calculateTotal(data2))
+  console.log(calculateAverage(data2))
+  console.log(findPositive(data2))
+  console.log(findMax(data2))
+} catch (error) {
+  console.log(error);
+} finally {
+  console.log("try again 😒")
+}
+
+// try {
+//   console.log(findPositive(data1));
+// } catch (error) {
+//   console.log(error);
+// }
 
 // console.log(calculateTotal(data1));
 // console.log(calculateTotal(data2));
-try {
-  console.log(calculateAverage(data1))
-} catch (error) {
-  console.log(error);
-}
-// console.log(calculateAverage(data1))
-console.log(calculateAverage(data2))
+
+// try {
+//   console.log(calculateAverage(data1))
+// } catch (error) {
+//   console.log(error);
+// }
+// try {
+//   console.log(calculateAverage(data2))
+// } catch (error) {
+//   console.log(error);
+// }
+
